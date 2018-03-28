a9651dcfc04848ea265aed7f6f7e7be7088f8136
Tarun Saha <taruntapansaha@gmail.com>
2018-03-27 20:04:43 -0500

    Simple Rest controller added

---
+++ restful-web-service/src/main/java/com/tarun/rest/webservices/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.tarun.rest.webservices.restfulwebservice;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	
+}
---
+++ restful-web-service/src/main/java/com/tarun/rest/webservices/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.tarun.rest.webservices.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+//	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World From the Bean!! ");
+	}
+
+}
